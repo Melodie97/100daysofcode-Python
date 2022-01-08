989e9d16eebded4fced1b4499e291ba679595b6a
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-08 23:00:02 +0100

    Day 1 of #100daysofcode
    Creating a band name generator

---
+++ band_name_generatator.py
@@ -0,0 +1,10 @@
+print("Welcome to the band name generator?")
+
+city = input("Enter your city you grew up in: \n ")
+
+pet = input("Enter your pet's name: \n ")
+
+band_name = Name1 + " "+Name2
+
+print("Congratulations! Your new band name is "+ band_name)
+
