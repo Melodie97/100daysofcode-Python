a0c661a445a62b16ffd7e172eae1395d59b8d1bf
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-10 22:01:11 +0100

    Day 3 of #100daysofcode
    Modified the BMI calculator

---
+++ Day 3/bmi2.py
@@ -0,0 +1,15 @@
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+#Write your code below this line 👇
+bmi = round(weight/height**2)
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif 18.5 < bmi <= 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif 25 < bmi <= 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif 30 < bmi <= 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
