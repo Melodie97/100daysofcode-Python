706787f7f1e04101cb03eefacfd48968deddbd51
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-11 18:56:51 +0100

    Day 4 0f #100daysofcode

--- Day 4/banker_roulette.py
+++ Day 4/banker_roulette.py
@@ -7,5 +7,6 @@ random.seed(test_seed)
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-random_name = random.choice(names)
-print(f"{random_name} is going to buy the meal today!")
+num_names = len(names)
+random_choice = random.randint(0, num_names-1)
+print(f"{random_choice} is going to buy the meal today!")
