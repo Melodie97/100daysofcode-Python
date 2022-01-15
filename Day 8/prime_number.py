296d3d6e0ac59ccb2405082a8110776f5ceb2638
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-16 00:32:40 +0100

    Day 8 of #100daysofcode

---
+++ Day 8/prime_number.py
@@ -0,0 +1,13 @@
+def prime_checker(number):
+    is_prime = True
+    for n in range(2, number):
+        if number % n == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
