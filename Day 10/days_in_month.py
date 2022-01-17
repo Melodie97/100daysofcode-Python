f4fe8b95a35f5e5ef6d5f2e6ba5239e7641cdaf6
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-17 23:02:16 +0100

    Day 10 of #100daysofcode

---
+++ Day 10/days_in_month.py
@@ -0,0 +1,30 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+    if is_leap(year) == True:
+        if month == 2:
+            return 29
+        else:
+            confirmed_month = month_days[month - 1]
+            return confirmed_month
+    else:
+        confirmed_month = month_days[month - 1]
+        return confirmed_month 
+
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
