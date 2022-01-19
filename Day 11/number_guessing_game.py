b5edec5954f2eb0aa7bbe97e68903b6d9ce7cc07
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-19 22:07:01 +0100

    Day 11 of #100daysofcode

---
+++ Day 11/number_guessing_game.py
@@ -0,0 +1,50 @@
+import random
+from art import logo
+
+numbers = range(1, 101)
+computer_guess = random.choice(numbers)
+print("Welcome to the number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+
+def play_game():
+
+    print(logo)
+
+    difficulty = input("Choose a difficulty level. Type 'easy' or 'hard': ")
+    continue_game = True
+    if difficulty == "easy":
+        attempts = 10
+    else:
+        attempts = 5
+
+    while continue_game:
+        print(f"You have {attempts} attempts remaining to guess the number")
+        if attempts == 0:
+            print("You lose")
+            continue_game = False
+        else:
+            guess = int(input("Make a guess: "))
+            if difficulty == 'easy':
+                if guess > computer_guess:
+                    attempts -= 1
+                    print (f"Too high \nGuess again")
+                elif guess < computer_guess:
+                    attempts -= 1
+                    print (f"Too low \nGuess again")
+                else:
+                    print("You Win!")
+                    continue_game = False
+            else:
+                if guess > computer_guess:
+                    attempts -= 1
+                    print (f"Too high \nGuess again")
+                elif guess < computer_guess:
+                    attempts -= 1
+                    print (f"Too low \nGuess again")
+                else:
+                    print("You Win!")
+                    continue_game = False
+
+       
+
+play_game()
