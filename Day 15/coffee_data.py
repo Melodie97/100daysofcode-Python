35b33e3c3a86393cbf58147a5c72c22898454197
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-24 22:09:18 +0100

    Day 15 of #100daysofcode

---
+++ Day 15/coffee_data.py
@@ -0,0 +1,37 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+prices = {
+    "espresso": 1.5,
+    "latte": 2.5,
+    "cappuccino": 3.5
+}
