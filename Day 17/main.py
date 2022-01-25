f4ad300a37ca339f1632e4509b281372c6c12e6e
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2022-01-26 00:30:26 +0100

    Day 17 of #100daysofcode

---
+++ Day 17/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for i in range(0, len(question_data)):
+    question_bank.append(Question(question_data[i]['question'], question_data[i]['correct_answer']))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
