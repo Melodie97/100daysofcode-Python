78577ced480a690d84d94cbfdb07a835334140f0
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-09 22:23:28 +0100

    Calculations

--- Day 5/average_height.py
+++ Day 5/average_height.py
@@ -3,14 +3,13 @@ for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 
 sum_heights = 0
+num_of_students = 0
 for height in student_heights:
     sum_heights += height
-print(sum_heights)
-
-num_of_students = 0
-for num in num_of_students:
     num_of_students += 1
+    
+print(sum_heights)
 print(num_of_students)
 
 avg_height = round(sum_heights/num_of_students)
-print(avg_height))
+print(avg_height)
