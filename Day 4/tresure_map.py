acba2ad460828e9f99129cf9fb0938bcefc06f33
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-09 22:06:52 +0100

    Using Random Library

--- Day 4/tresure_map.py
+++ Day 4/tresure_map.py
@@ -2,6 +2,7 @@
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
+
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
