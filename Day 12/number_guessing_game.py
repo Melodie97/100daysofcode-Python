8a93cb0ac0ed2a656c39ba23d63c4d23c168712d
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-12 20:03:50 +0100

    number guessing game

--- Day 12/number_guessing_game.py
+++ Day 12/number_guessing_game.py
@@ -3,6 +3,7 @@ from art import logo
 
 numbers = range(1, 101)
 computer_guess = random.choice(numbers)
+
 print("Welcome to the number Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
 
