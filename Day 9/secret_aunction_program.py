385c06d06dac19a5d8bcdea7a9c74a9747caef0b
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-12 19:49:05 +0100

    Secret auction, grading scores

--- Day 9/secret_aunction_program.py
+++ Day 9/secret_aunction_program.py
@@ -12,6 +12,7 @@ def find_highest_bidder(bidding_record):
     if bid_amount > highest_bid: 
       highest_bid = bid_amount
       winner = bidder
+      
   print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 while not bidding_finished:
