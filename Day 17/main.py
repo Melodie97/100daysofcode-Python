07ca0d5699b1bc46423b24d9b027c8a8e6672add
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-18 17:39:44 +0100

    Quiz bank

--- Day 17/main.py
+++ Day 17/main.py
@@ -7,7 +7,6 @@ for i in range(0, len(question_data)):
     question_bank.append(Question(question_data[i]['question'], question_data[i]['correct_answer']))
 
 quiz = QuizBrain(question_bank)
-
 while quiz.still_has_questions():
     quiz.next_question()
 
