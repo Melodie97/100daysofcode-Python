aa7a6bc6ca084cca81e26544e92fdb311944836d
Melodie Ezeani <78446940+Melodie97@users.noreply.github.com>
2023-02-26 22:07:13 +0100

    Car manager

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -5,7 +5,6 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-
 class CarManager():
     def __init__(self):
         self.cars = []
